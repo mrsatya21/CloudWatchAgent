e549524c975ae0e522fc76736903a38d25c74703
Kumar Satyanshu <kkumarsatyanshu2107@gmail.com>
2024-08-16 19:19:05 +0530

    Made changes in the congiguration file

--- configuration.sh
+++ configuration.sh
@@ -3,29 +3,70 @@
 # Set the function to create config.json file
 configfunction(){
     cat <<EOT > config.json
-    {
-        "agent": {
-            "metrics_collection_interval": 60,
-            "run_as_user": "cwagent"
-        },
-        "metrics": {
-            "metrics_collected": {
-                "mem": {
-                    "measurement": ["used_percent", "total", "used"],
-                    "metrics_collection_interval": 30
-                },
-                "cpu": {
-                    "measurement": ["cpu_usage_idle", "cpu_usage_user", "cpu_usage_system"],
-                    "metrics_collection_interval": 60,
-                    "totalcpu": true
-                },
-                "disk": {
-                    "measurement": ["used_percent", "used", "total"],
-                    "metrics_collection_interval": 60,
-                    "resources": ["*"]
-                }
-            }
-        }
-    }
+{
+	"agent": {
+		"metrics_collection_interval": 60,
+		"run_as_user": "cwagent"
+	},
+	"metrics": {
+		"aggregation_dimensions": [
+			[
+				"InstanceId"
+			]
+		],
+		"append_dimensions": {
+			"AutoScalingGroupName": "${aws:AutoScalingGroupName}",
+			"ImageId": "${aws:ImageId}",
+			"InstanceId": "${aws:InstanceId}",
+			"InstanceType": "${aws:InstanceType}"
+		},
+		"metrics_collected": {
+			"cpu": {
+				"measurement": [
+					"cpu_usage_idle",
+					"cpu_usage_iowait",
+					"cpu_usage_user",
+					"cpu_usage_system"
+				],
+				"metrics_collection_interval": 60,
+				"resources": [
+					"*"
+				],
+				"totalcpu": false
+			},
+			"disk": {
+				"measurement": [
+					"used_percent",
+					"inodes_free"
+				],
+				"metrics_collection_interval": 60,
+				"resources": [
+					"*"
+				]
+			},
+			"diskio": {
+				"measurement": [
+					"io_time"
+				],
+				"metrics_collection_interval": 60,
+				"resources": [
+					"*"
+				]
+			},
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 60
+			},
+			"swap": {
+				"measurement": [
+					"swap_used_percent"
+				],
+				"metrics_collection_interval": 60
+			}
+		}
+	}
+}
 EOT
 }
